8ac4bd949ffe4b30baf856c0087a170af79cb60c
abhiyan bhattarai <abhiyanbhattarai88@gmail.com>
2023-10-16 12:50:13 -0400

    requirements.txt changes

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from website import create_app
 
+from flask import redirect, url_for
+
 app = create_app()
+
+# Redirect to the login page when the app is started
+@app.route('/login')
+def index():
+    return redirect(url_for('auth.login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
